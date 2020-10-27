09e21e4ee48bf93f69625ad170524f906f70c61a
Viktorlazi <viktorlazic11@gmail.com>
2020-10-27 18:26:26 +0100

    first commit

---
+++ src/components/home/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Contact = () => {
+    return (
+        <div className="homeContact">
+            <ul>
+                <li className="telephoneNumber">+385 12 3456789</li>
+                <li className="email">info@vilaz.com</li>
+            </ul>
+        </div>
+    )
+}
