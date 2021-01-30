8bac4d06a6dcc0278b62a47e7f9ade817b4f35c5
Viktorlazi <viktorlazi010@gmail.com>
2021-01-30 11:39:37 +0100

    from scratch

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import './contact.css'
+import 'fontawesome'
 
 export const Contact = () => {
     return (
         <div className="homeContact">
             <ul>
-                <li className="telephoneNumber">+385 12 3456789</li>
-                <li className="email">info@vilaz.com</li>
+                <li>
+                    <i className="fa fa-phone" aria-hidden="true"></i>
+                    <a className="telephoneNumber">+385 12 3456789</a>
+                </li>
+                <li>
+                    <i className="fa fa-envelope" aria-hidden="true"></i>
+                    <a className="email">info@vilaz.com</a>
+                </li>
             </ul>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Contact } from '../components/home/Contact'
+import './home.css'
 
 export default class Home extends Component {
     render() {
         return (
             <div className="Home">
-                
+
             </div>
         )
     }
